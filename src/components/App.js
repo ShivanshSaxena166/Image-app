ea5af2ff0855a283dbf42d766d3b23d175f8fc59
unknown <shivanshkumar166@gmail.com>
2020-11-08 20:49:56 +0530

    fixed a bug

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,24 @@ import SearchBar from './SearchBar'
 import axios from 'axios'
 class App extends React.Component
 {
-onSearchSubmit(term)
+    state = { images:[] }
+    async onSearchSubmit(term)
 {
     console.log(term);
-    axios.get('https://api.unsplash.com/search/photos',{
-params:{query:term}     ,   
+   const response= await  axios.get('https://api.unsplash.com/search/photos',{
+params:{query:term},   
 headers:{
     Authorization:'Client-ID O2Fs_p35SpnZvx7sp_TYC4rUPm1ZbGOmWI3zuOTGnz4'
 }
 
     })
+    this.setState({images:response.data.results})
 }    
     render(){
 return(<div className="ui container" style={{marginTop:'10px'}}>
     <SearchBar onSubmit={this.onSearchSubmit}/>
+    Found:{this.state.images.length} images
+
 </div>)
 }}
 export default App;
